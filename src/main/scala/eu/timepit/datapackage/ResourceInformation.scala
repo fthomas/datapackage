1ad21c4383ce8db1e64ce4cddd76864365b95aa8
Frank S. Thomas <frank@timepit.eu>
2016-08-17 00:42:08 +0200

    Ugly De/Encoder for ResourceInformation

--- src/main/scala/eu/timepit/datapackage/ResourceInformation.scala
+++ src/main/scala/eu/timepit/datapackage/ResourceInformation.scala
@@ -1,14 +1,28 @@
 package eu.timepit.datapackage
 
-import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto._
+import io.circe.{Decoder, Encoder, Json}
 
-final case class ResourceInformation()
+final case class ResourceMetadata()
+
+final case class ResourceInformation(location: ResourceLocation,
+                                     name: Option[String] = None)
 
 object ResourceInformation {
   implicit val resourceInformationDecoder: Decoder[ResourceInformation] =
-    deriveDecoder
+    Decoder.instance { c =>
+      for {
+        loc <- Decoder[ResourceLocation].apply(c)
+        name <- c.downField("name").as[Option[String]]
+      } yield ResourceInformation(loc, name)
+    }
 
   implicit val resourceInformationEncoder: Encoder[ResourceInformation] =
-    deriveEncoder
+    deriveEncoder[ResourceInformation].mapJson { x =>
+      val o = x.asObject.get
+      val loc = o.apply("location").get
+      val o2 = o.remove("location")
+
+      Json.fromJsonObject(o2).deepMerge(loc)
+    }
 }
--- src/test/scala/eu/timepit/datapackage/ResourceInformationSpec.scala
+++ src/test/scala/eu/timepit/datapackage/ResourceInformationSpec.scala
@@ -1,6 +1,8 @@
 package eu.timepit.datapackage
 
 import cats.data.Xor
+import eu.timepit.datapackage.ResourceLocation.Url
+import io.circe.Printer
 import io.circe.parser.decode
 import io.circe.syntax._
 import org.scalacheck.Prop._
@@ -9,14 +11,27 @@ import org.scalacheck.Properties
 class ResourceInformationSpec
     extends Properties(classOf[ResourceInformation].getSimpleName) {
   {
-    val res = ResourceInformation()
-    val json = """{}"""
+    val res = ResourceInformation(Url("test"))
+    val json = """{"url":"test"}"""
 
     property("encode minimal resource") = secure {
-      res.asJson.noSpaces ?= json
+      res.asJson.pretty(Printer.noSpaces.copy(dropNullKeys = true)) ?= json
     }
 
     property("decode minimal resource") = secure {
+      decode[ResourceInformation](json) ?= Xor.right(res)
+    }
+  }
+
+  {
+    val res = ResourceInformation(Url("test"), Some("data"))
+    val json = """{"url":"test","name":"data"}"""
+
+    property("encode resource with name") = secure {
+      res.asJson.pretty(Printer.noSpaces.copy(dropNullKeys = true)) ?= json
+    }
+
+    property("decode resource with name") = secure {
       decode[ResourceInformation](json) ?= Xor.right(res)
     }
   }
