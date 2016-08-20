3cb4ab7afea2497240c9574820702e118df6cee0
Frank S. Thomas <frank@timepit.eu>
2016-08-20 13:35:11 +0200

    Test encodingOrDefault

--- src/test/scala/eu/timepit/datapackage/ResourceMetadataSpec.scala
+++ src/test/scala/eu/timepit/datapackage/ResourceMetadataSpec.scala
@@ -1,9 +1,19 @@
 package eu.timepit.datapackage
 
 import eu.timepit.datapackage.testUtil._
+import org.scalacheck.Prop._
 import org.scalacheck.Properties
 
 class ResourceMetadataSpec extends Properties(nameOf[ResourceMetadata]) {
   property("JSON round-trip") = jsonRoundTrip[ResourceMetadata]
   property("JSON decoding error") = jsonDecodeNull[ResourceMetadata]
+
+  property("encodingOrDefault 1") = secure {
+    ResourceMetadata().encodingOrDefault ?= "UTF-8"
+  }
+
+  property("encodingOrDefault 2") = secure {
+    val ascii = "ASCII"
+    ResourceMetadata(encoding = Some(ascii)).encodingOrDefault ?= ascii
+  }
 }
