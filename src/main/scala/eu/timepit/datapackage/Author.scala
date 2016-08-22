6937df96fbf7e77327bb95d1fba3e5cf1626b4a2
Frank S. Thomas <frank@timepit.eu>
2016-08-22 02:24:56 +0200

    Add failing test

--- src/main/scala/eu/timepit/datapackage/Author.scala
+++ src/main/scala/eu/timepit/datapackage/Author.scala
@@ -5,7 +5,12 @@ import io.circe.{Decoder, Encoder}
 
 final case class Author(name: String,
                         web: Option[String] = None,
-                        email: Option[String] = None)
+                        email: Option[String] = None) {
+
+  def inline: String =
+    (name :: List(email.map(e => s"<$e>"), web.map(w => s"($w)")).flatten)
+      .mkString(" ")
+}
 
 object Author {
   implicit val decodeAuthor: Decoder[Author] = deriveDecoder
--- src/test/scala/eu/timepit/datapackage/AuthorSpec.scala
+++ src/test/scala/eu/timepit/datapackage/AuthorSpec.scala
@@ -1,9 +1,16 @@
 package eu.timepit.datapackage
 
+import cats.data.Xor
 import eu.timepit.datapackage.testUtil._
+import io.circe.parser
+import org.scalacheck.Prop._
 import org.scalacheck.Properties
 
 class AuthorSpec extends Properties(nameOf[Author]) {
   property("JSON round-trip") = jsonRoundTrip[Author]
   property("JSON decoding error") = jsonDecodeNull[Author]
+
+  property("decode from inline string") = forAll { (a: Author) =>
+    parser.decode[Author](a.inline) ?= Xor.Right(a)
+  }
 }
