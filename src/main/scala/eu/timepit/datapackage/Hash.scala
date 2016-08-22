fabd3fb092222b8e4d95e05b4cf3cdc4beaf7b74
Frank S. Thomas <frank@timepit.eu>
2016-08-22 18:24:42 +0200

    Simplify Hash decoder

--- src/main/scala/eu/timepit/datapackage/Hash.scala
+++ src/main/scala/eu/timepit/datapackage/Hash.scala
@@ -1,29 +1,27 @@
 package eu.timepit.datapackage
 
 import cats.data.Xor
-import eu.timepit.refined.auto._
 import eu.timepit.datapackage.types.NonEmptyString
 import eu.timepit.refined.api.RefType
-import io.circe.{Decoder, DecodingFailure, Encoder, Json}
+import eu.timepit.refined.auto._
+import io.circe.{Decoder, Encoder, Json}
 
 final case class Hash(digest: NonEmptyString,
                       algorithm: Option[NonEmptyString] = None)
 
 object Hash {
   implicit val decodeHash: Decoder[Hash] =
-    Decoder.instance { c =>
-      Decoder.decodeString(c).flatMap { str =>
-        val parts = str.split(":", 2).toList
-        val nonEmptyParts =
-          parts.flatMap(RefType.applyRef[NonEmptyString](_).right.toOption)
-        nonEmptyParts match {
-          case digest :: Nil =>
-            Xor.Right(Hash(digest))
-          case algorithm :: digest :: Nil =>
-            Xor.Right(Hash(digest, Some(algorithm)))
-          case _ =>
-            Xor.Left(DecodingFailure(s"Hash: invalid data: $str", c.history))
-        }
+    Decoder.decodeString.emap { str =>
+      val parts = str.split(":", 2).toList
+      val nonEmptyParts =
+        parts.flatMap(RefType.applyRef[NonEmptyString](_).right.toOption)
+      nonEmptyParts match {
+        case digest :: Nil =>
+          Xor.Right(Hash(digest))
+        case algorithm :: digest :: Nil =>
+          Xor.Right(Hash(digest, Some(algorithm)))
+        case _ =>
+          Xor.Left(s"Hash: invalid data: $str")
       }
     }
 
